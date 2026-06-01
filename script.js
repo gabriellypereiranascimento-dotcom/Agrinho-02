786f559dc8c4b39c44c6136033c8fe0528d57021
gabriellypereiranascimento-dotcom <gabrielly.pereira.nascimento@escola.pr.gov.br>
2026-06-01 10:24:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+    "🌎 A sustentabilidade ajuda a proteger a natureza e garantir alimentos para as futuras gerações!";
+}
